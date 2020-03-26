ccc652e3308b1d07dd11e8019a123ea80bee617d
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-26 18:16:37 -0500

    Fourth test passes

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -1,6 +1,10 @@
 class Student:
     """Student class"""
     def __init__(self, lname, fname, major, gpa=0.0):
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        gpa_characters = set("1234567890.")
+        if not (name_characters.issuperset(lname)):
+            raise ValueError
         self.last_name = lname
         self.first_name = fname
         self.major = major
