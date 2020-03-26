3e759730dca14706cc05bd1d82a9f670cc6bbee2
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-26 18:24:01 -0500

    Sixth test passes

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -3,7 +3,7 @@ class Student:
     def __init__(self, lname, fname, major, gpa=0.0):
         name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
         gpa_characters = set("1234567890.")
-        if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
+        if not (name_characters.issuperset(lname) and name_characters.issuperset(fname) and name_characters.issuperset(major)):
             raise ValueError
         self.last_name = lname
         self.first_name = fname
@@ -11,4 +11,4 @@ class Student:
         self.gpa = gpa
 
     def __str__(self):
-        return self.last_name + ", " + self.first_name + " has major " + self.major + "with gpa: " + str(self.gpa)
+        return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
